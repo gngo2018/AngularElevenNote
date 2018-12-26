77fed0ac5fee5b15a70623239db2c8937276b277
gngo2018 <gngo2018@gmail.com>
2018-12-26 16:28:04 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+
+import {
+        MatToolbarModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+const routes = [
+  { path: 'register', component: RegistrationComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
